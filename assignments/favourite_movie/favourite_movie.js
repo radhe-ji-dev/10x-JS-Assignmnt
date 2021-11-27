e7bee1ebc24c72e8bdfe2ee73209bed275c5146b
Radhakant Tiwari <therktiwadi@gmail.com>
2021-11-27 16:05:30 +0530

    fav movie done

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -1,7 +1,13 @@
 const movies = [];
 
 function favouriteMovie(operation, movie) {
-    // Write your code here
+  if (operation == "add") {
+    movies.push(movie);
+  }
+  if (operation == "remove") {
+    movies.pop();
+  }
+  return movies;
 }
 
 module.exports = favouriteMovie;
